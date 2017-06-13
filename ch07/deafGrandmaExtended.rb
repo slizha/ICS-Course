b7c4da342e4185a34f70c3addeacdc761380c6c2
Sarah Hammond <s.hammond@berkeley.edu>
2017-06-13 10:08:55 -0700

    Week 4 Assignment

--- ch07/deafGrandmaExtended.rb
+++ ch07/deafGrandmaExtended.rb
@@ -4,12 +4,12 @@ num = 0
 while num < 2
   if resp == 'BYE'
     num += 1
-    elsif resp == resp.upcase
-      num = 0
-      puts 'NO, NOT SINCE ' + (rand(21) + 1930).to_s + '!'
-      else
-        num = 0
-        puts 'HUH?! SPEAK UP, SONNY!'
+  elsif resp == resp.upcase
+    num = 0
+    puts 'NO, NOT SINCE ' + (rand(21) + 1930).to_s + '!'
+  else
+    num = 0
+    puts 'HUH?! SPEAK UP, SONNY!'
   end
   resp = gets.chomp
 end
