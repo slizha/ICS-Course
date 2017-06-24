cf730ed51da739c30a0967209d764d70d78001a9
Sarah Hammond <s.hammond@berkeley.edu>
2017-06-23 23:50:57 -0700

    Week 6 Assignment

--- ch10/99BottlesOfBeer.rb
+++ ch10/99BottlesOfBeer.rb
@@ -1,12 +1,11 @@
 def english_number number
-  if number < 0 # No negative numbers.
+  if number < 0
     return 'Please enter a number that isn\'t negative.'
   end
   if number == 0
     return 'zero'
   end
-  # No more special cases! No more returns!
-  num_string = '' # This is the string we will return.
+  num_string = ''
   ones_place = ['one', 'two', 'three',
                 'four', 'five', 'six',
                 'seven', 'eight', 'nine']
@@ -16,12 +15,16 @@ def english_number number
   teenagers = ['eleven', 'twelve', 'thirteen',
                'fourteen', 'fifteen', 'sixteen',
                'seventeen', 'eighteen', 'nineteen']
-  # "left" is how much of the number
-  # we still have left to write out.
-  # "write" is the part we are
-  # writing out right now.
-  # write and left...get it? :)
   left = number
+  write = left/1000000000000
+  left = left - write*1000000000000
+  if write > 0
+    trillions = english_number write
+    num_string = num_string + trillions + ' trillion'
+    if left > 0
+      num_string = num_string + ' '
+    end
+  end
   write = left/1000000000
   left = left - write*1000000000
   if write > 0
@@ -64,33 +67,20 @@ def english_number number
   left = left - write*10 # Subtract off those tens.
   if write > 0
     if ((write == 1) and (left > 0))
-      # Since we can't write "tenty-two" instead of
-      # "twelve", we have to make a special exception
-      # for these.
       num_string = num_string + teenagers[left-1]
-      # The "-1" is because teenagers[3] is
-      # 'fourteen', not 'thirteen'.
-      # Since we took care of the digit in the
-      # ones place already, we have nothing left to write.
       left = 0
     else
       num_string = num_string + tens_place[write-1]
-      # The "-1" is because tens_place[3] is
-      # 'forty', not 'thirty'.
     end
     if left > 0
-      # So we don't write 'sixtyfour'...
       num_string = num_string + '-'
     end
   end
-  write = left # How many ones left to write out?
-  left = 0 # Subtract off those ones.
+  write = left
+  left = 0
   if write > 0
     num_string = num_string + ones_place[write-1]
-    # The "-1" is because ones_place[3] is
-    # 'four', not 'three'.
   end
-  # Now we just return "num_string"...
   return num_string
 end
 bottles = 9999
